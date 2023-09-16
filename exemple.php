e56ae9c2d85d94d7d7e872315676f2d678fb9c7b
MedAzizGhanmi7 <ghanmimohamedaziz5@gmail.com>
2023-09-16 12:35:16 +0100

    exemple3

--- exemple.php
+++ exemple.php
@@ -1,5 +1,20 @@
 <?PHP
-$var="Bonjour";
+/*$var="Bonjour";
 echo $var;
-echo "je suis du code php" ;
+echo "je suis du code php" ;*/
+
+/*exemple 3*/
+
+$age =30;
+$sex ="femme";
+
+if ($sex==="femme" && $age>=21 && $age<=40)
+{
+    echo "bienvenue vous etes une {$sex} de {$age} ans";
+}
+else {
+    echo "Désolé";
+}
+
+
 ?>
